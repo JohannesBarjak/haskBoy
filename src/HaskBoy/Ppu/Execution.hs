64a9e45467ae68d783aa78f59f5ec0d9665b2216
Johannes <johannes.barjak@gmail.com>
2024-05-23 17:01:06 -0500

    Separate pure logic from sprite drawing code

--- src/HaskBoy/Ppu/Execution.hs
+++ src/HaskBoy/Ppu/Execution.hs
@@ -6,7 +6,7 @@ module HaskBoy.Ppu.Execution
     ( ppuCycle
     , drawTiles
     , bgScanline
-    , getTileRow, tileRow
+    , tileRow
     , scx, scy
     , ly, lyc
     , ppuMode
@@ -20,7 +20,6 @@ import Data.Bits ((.&.), shiftR, (.|.))
 import Data.Bool (bool)
 
 import Data.Function (on)
-import Data.Foldable (asum)
 import Data.Ix (inRange)
 import Data.Maybe (fromMaybe)
 
@@ -60,42 +59,35 @@ drawTiles :: State Emulator ()
 drawTiles = do
     lineY <- fromIntegral <$> use (mmu.ly)
     ppu.display.ix lineY <~ bgScanline <$> use mmu
-    drawSprites
+    drawSprites . spriteScan =<< use mmu
 
 -- TODO: Implement 40 sprite limit.
-drawSprites :: State Emulator ()
-drawSprites = do
-    size <- bool 8 16 <$> use (mmu.objSize)
+drawSprites :: Seq (ObjAttr, Seq Pixel) -> State Emulator ()
+drawSprites sprites = forM_ sprites $ \(obj, srow) -> do
+    let writeSprite i v = fromMaybe v $
+            S.lookup (i - fromIntegral (obj^.xPos) + 8) srow
+
     lineY <- use (mmu.ly)
+    ppu.display.ix (fromIntegral lineY) %= S.mapWithIndex writeSprite
 
-    -- Only Y visibility affects the maximum of 10 sprite objects per scanline limit.
-    let visibleY obj = inRange ((lineY + 1 - size, lineY)&both +~ 16) (obj^.yPos)
-    scanAttrs <- S.take 10 . S.filter visibleY <$> use (mmu.oam)
+spriteScan :: Mmu -> Seq (ObjAttr, Seq Pixel)
+spriteScan mem = scanAttr <&> (,) <*> ((tileRow mem . ri) <*> ti)
 
-    tileAddrMode <- bool 0xFF 0xFE <$> use (mmu.objSize)
-    let getTileAddr v = 0x8000 + (fromIntegral (v .&. tileAddrMode) * 16)
+    where scanAttr = S.take 10 $ S.filter visibleY (mem^.oam)
+          visibleY obj = inRange ((mem^.ly + 1 - size, mem^.ly)&both +~ 16) (obj^.yPos)
+          size = bool 8 16 (mem^.objSize)
 
-    forM_ scanAttrs $ \obj -> do
-        let rowIndex = size - (obj^.yPos - lineY + size - 16)
-        let tileIndex = getTileAddr (obj^.tlIdx)
-
-        spriteRow <- zoom mmu $ getTileRow rowIndex tileIndex
-
-        let writeSprite i v = fromMaybe v $
-                S.lookup (i - fromIntegral (obj^.xPos) + 8) spriteRow
-
-        ppu.display.ix (fromIntegral lineY) %= S.mapWithIndex writeSprite
+          ti obj = 0x8000 + (fromIntegral (obj^.tlIdx .&. addrMode) * 16)
+          ri obj = size - (obj^.yPos - (mem^.ly) + size - 16)
+          addrMode = bool 0xFF 0xFE (mem^.objSize)
 
 bgScanline :: Mmu -> Seq Pixel
 bgScanline mem
-    = (tileRows . tileAddress =<< bgTileMaps)
+    = (tileRow mem ri . tileAddress =<< bgTileMaps)
     & S.drop (fromIntegral $ mem^.scx)
     & S.cycleTaking 160
 
-    where tileRows ta = let ra = ta + (fromIntegral ri * 2) in
-            tileRow (mem^.cloneLens (addr ra)) (mem^.cloneLens (addr $ ra + 1))
-
-          bgTileMaps = let ta = 0x9800 + fromIntegral ti * 32 in
+    where bgTileMaps = let ta = 0x9800 + fromIntegral ti * 32 in
             [mem^.cloneLens (addr i) | i <- [ta..ta + 32]]
 
           tileAddress idx
@@ -104,20 +96,18 @@ bgScanline mem
 
           (ti, ri) = (mem^.ly + mem^.scy) `quotRem` 8
 
-getTileRow :: Word8 -> Address -> State Mmu (Seq Pixel)
-getTileRow ri ta
-    = let ra = ta + (fromIntegral ri * 2) in tileRow
-        <$> use (cloneLens $ addr ra)
-        <*> use (cloneLens $ addr (ra + 1))
+tileRow :: Mmu -> Word8 -> Address -> Seq Pixel
+tileRow mem ri ta = let ra = ta + (fromIntegral ri * 2) in
+    buildRow (mem^.cloneLens (addr ra)) (mem^.cloneLens (addr $ ra + 1))
+
+    -- | Get a single tile row from a pair of bytes
+    where buildRow :: Word8 -> Word8 -> Seq Pixel
+          buildRow v1 v2 = [on toPixel (toBool . (.&. 1) . (`shiftR` i)) v1 v2 | i <- [7,6..0]]
 
 twoCompl :: Word8 -> Int
 twoCompl r8
     | r8 < 128  = fromIntegral r8
     | otherwise = -(256 - fromIntegral r8)
-
--- | Get a single tile row from a pair of bytes
-tileRow :: Word8 -> Word8 -> Seq Pixel
-tileRow v1 v2 = [on toPixel (toBool . (.&. 1) . (`shiftR` i)) v1 v2 | i <- [7,6..0]]
 
 ppuMode :: Lens' Mmu PpuMode
 ppuMode = lens _ppuMode $ \mem v ->
@@ -127,7 +117,7 @@ ppuMode = lens _ppuMode $ \mem v ->
           _ppuMode mem = toEnum . fromIntegral $ (mem^?!ioreg.ix 0x41) .&. 3
 
 lcdStat :: Lens' Mmu Word8
-lcdStat = lens (^?!ioreg.ix 0x41) (\mem v -> mem&ioreg.ix 0x41 .~ v)
+lcdStat = cloneLens $ raw 0xFF41
 
 scx, scy :: Lens' Mmu Word8
 scx = lens (^?!ioreg.ix 0x43) (\mem v -> mem&ioreg.ix 0x43 .~ v)
@@ -137,7 +127,7 @@ lyc :: Lens' Mmu Word8
 lyc = lens (^?!ioreg.ix 0x45) (\mem v -> mem&ioreg.ix 0x45 .~ v)
 
 ly :: Lens' Mmu Word8
-ly = lens (^?!ioreg.ix 0x44) (\mem v -> mem&ioreg.ix 0x44 .~ v)
+ly = cloneLens (raw 0xFF44)
 
 objEnable, objSize, bgTileData, winEnable, lcdEnable :: Lens' Mmu Bool
 
